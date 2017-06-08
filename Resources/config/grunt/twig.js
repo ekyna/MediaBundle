426b13884c55f47ad23b9bbea27d7d0a155371db
Etienne Dauvergne <contact@ekyna.com>
2017-06-08 14:08:35 +0200

    [Media] Updated twigJs, rebuild assets.

--- Resources/config/grunt/twig.js
+++ Resources/config/grunt/twig.js
@@ -1,15 +1,15 @@
 module.exports = function (grunt, options) {
     return {
-        options: {
-            amd_wrapper: true,
-            amd_define: 'ekyna-media-templates',
-            variable: 'templates',
-            template_key: function(path) {
-                var split = path.split('/');
-                return split[split.length-1];
-            }
-        },
         media: {
+            options: {
+                amd_wrapper: true,
+                amd_define: 'ekyna-media-templates',
+                variable: 'templates',
+                template_key: function(path) {
+                    var split = path.split('/');
+                    return split[split.length-1];
+                }
+            },
             files: {
                 'src/Ekyna/Bundle/MediaBundle/Resources/public/js/ekyna-media-templates.js': [
                     'src/Ekyna/Bundle/MediaBundle/Resources/views/thumb.html.twig'
